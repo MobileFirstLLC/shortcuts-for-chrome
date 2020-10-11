5796bb37d8cbecdb0287d5c7779bc331da649d07
Neea Rusch <neea.rusch@mobilefirst.me>
2020-10-11 14:50:22 -0400

    fix: update menu links

--- src/config.js
+++ src/config.js
@@ -5,29 +5,23 @@
  * ./assest/locales/en/links.json. If the url contains "-" or "/"
  * replace such chars with underscore "_" in the i18n dictionary.
  */
-export const MenuLinks = ['accessibility', 'appcache-internals', 'apps', 'blob-internals',
-    'bluetooth-internals', 'bookmarks', 'cache', 'chrome-urls', 'components',
-    'conflicts', 'crashes', 'credits', 'device-log', 'devices', 'dino', 'discards',
-    'dns', 'downloads', 'extensions', 'extensions/configureCommands', 'flags', 'flash',
-    'gcm-internals', 'gpu', 'help', 'histograms', 'history', 'identity-internals',
-    'indexeddb-internals', 'inspect', 'invalidations', 'local-state',
-    'media-internals', 'nacl', 'net-export', 'net-internals', 'network-errors',
-    'newtab', 'ntp-tiles-internals', 'omnibox', 'password-manager-internals',
-    'policy', 'predictors', 'print', 'profiler', 'quota-internals',
-    'serviceworker-internals', 'settings', 'settings/accessibility',
-    'settings/appearance', 'settings/autofill', 'settings/defaultBrowser',
-    'settings/importData', 'settings/languages', 'settings/onStartup',
-    'settings/passwordsAndForms', 'settings/people', 'settings/printing',
-    'settings/privacy', 'settings/reset', 'settings/search', 'settings/syncSetup',
-    'settings/system', 'signin-internals', 'site-engagement', 'suggestions',
-    'supervised-user-internals', 'sync-internals', 'system',
-    'taskscheduler-internals', 'terms', 'tracing',
-    'translate-internals', 'usb-internals', 'user-actions', 'version',
-    'view-http-cache', 'webrtc-internals', 'webrtc-logs', 'media-engagement',
-    'safe-browsing', 'settings/clearBrowserData', 'history/syncedTabs',
-    'download-internals', 'interstitials', 'interventions-internals',
-    'process-internals'];
-
+export const MenuLinks = [
+    "accessibility", "appcache-internals", "apps", "autofill-internals", "blob-internals", "bluetooth-internals", "bookmarks",
+    "chrome-urls", "components", "conversion-internals", "crashes", "credits", "device-log", "devices", "dino", "discards",
+    "download-internals", "downloads", "extensions", "extensions/configureCommands", "flags",
+    "gcm-internals", "gpu", "help", "histograms", "history", "history/syncedTabs", "identity-internals",
+    "indexeddb-internals", "inspect", "internals/web-app", "interstitials", "interventions-internals", "invalidations",
+    "local-state", "management", "media-engagement", "media-internals", "nacl", "net-export", "net-internals",
+    "network-errors", "newtab", "ntp-tiles-internals", "omnibox", "password-manager-internals",
+    "policy", "predictors", "prefs-internals", "print", "process-internals", "quota-internals", "safe-browsing",
+    "serviceworker-internals", "settings", "settings/accessibility", "settings/appearance",
+    "settings/autofill", "settings/clearBrowserData", "settings/defaultBrowser", "settings/importData",
+    "settings/languages", "settings/onStartup", "settings/passwords", "settings/people", "settings/printing",
+    "settings/privacy", "settings/reset", "settings/search", "settings/syncSetup", "settings/system",
+    "signin-internals", "site-engagement", "suggestions", "sync-internals", "system", "terms",
+    "tracing", "translate-internals", "ukm", "usb-internals", "user-actions", "version", "webrtc-internals",
+    "webrtc-logs"
+];
 
 /**
  * List of options to display in the context menu
