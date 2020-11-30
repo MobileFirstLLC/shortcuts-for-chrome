651b267cc618d77844fbc162c56d9cb83f09eb92
Neea Rusch <neea.rusch@mobilefirst.me>
2020-11-30 02:35:16 -0500

    comment class

--- src/background/onUpdate.js
+++ src/background/onUpdate.js
@@ -1,5 +1,11 @@
 import Storage from '../modules/storage';
 
+/**
+ * @class
+ * @name UpdateHandler
+ * @classdesc Register action that should occur on
+ * extension install or update.
+ */
 export default class UpdateHandler {
     constructor() {
         window.chrome.runtime.onInstalled.addListener(details => {
