37c22333bfee25e4e19c681eb5c7270de3853804
Neea Rusch <neea.rusch@mobilefirst.me>
2021-07-18 01:07:39 -0400

    update translation tools

--- i18n/translate.js
+++ i18n/translate.js
@@ -1,49 +1,31 @@
-/** * * * * * * * * * * * * * * * * * * * *
- * Shortcuts for Chrome
- * Custom navigation menu for Chrome browser
- *
- * Author: Mobile First LLC
- * Website: https://mobilefirst.me
- *
- * @description
- * Utility script for converting CSV files to localized dictionaries.
- *
- * This script takes a CSV file and converts that to multiple json files
- * formatted as expected by chrome browser extension.
- *
- * see: Localization message formats
- * {@link https://developer.chrome.com/docs/extensions/mv2/i18n-messages}
- * * * * * * * * * * * * * * * * * * * * */
+/**
+ * Utility script that formats PO Editor files as extension locales files
+ */
 
-const csv = require('csv-parser');
 const fs = require('fs');
 const path = require('path');
 
-const locales = {}, keys = [];
-const input = './assets/dictionary.csv';
-const outPath = './assets/locales/';
+const inDirectory = './i18n/';
+const outDirectory = './assets/locales/';
 const outFileName = 'messages.json';
+const firstLink = 'accessibility';
+const menuLinksDir = './src/';
+const menuLinksFile = 'links.json';
 
-let menuLinksStartRow = false,
-    rowCounter = 0;
+const ensureDirectoryExists = (dirPath) => fs.existsSync(dirPath) || fs.mkdirSync(dirPath);
 
-const ensureDirectoryExists = function (dirPath) {
-    if (!fs.existsSync(dirPath)) {
-        fs.mkdirSync(dirPath);
-    }
-};
+const sanitizedKey = key => key.replace(/[-\/]/g, '_');
 
-const writeFile = function (path, filename, obj) {
-    const content = JSON.stringify(obj, null, 2);
-
-    ensureDirectoryExists(path);
-    fs.writeFileSync(path + '/' + filename, content);
+const writeJSONFile = function (dirPath, filename, obj) {
+    ensureDirectoryExists(dirPath);
+    fs.writeFileSync(
+        path.join(dirPath, filename),
+        JSON.stringify(obj, null, 2)
+    );
 };
 
 const clearDirectory = function (directory) {
-    const files = fs.readdirSync(directory);
-
-    for (const file of files) {
+    for (const file of fs.readdirSync(directory)) {
         const p = path.join(directory, file);
 
         if (fs.lstatSync(p).isDirectory()) {
@@ -54,69 +36,29 @@ const clearDirectory = function (directory) {
     }
 };
 
-fs.createReadStream(input)
-    .pipe(csv())
+const writeMenuLinks = content => {
+    const keysArray = Object.keys(content);
+    const allLinks = {'MenuLinks': keysArray.splice(keysArray.indexOf(firstLink))};
 
-    // read CSV data one row at a time
-    .on('data', (row) => {
+    writeJSONFile(menuLinksDir, menuLinksFile, allLinks);
+    console.log('Updated menu links: ', Object.keys(allLinks.MenuLinks).length);
+};
 
-        // first row contains column keys i.e. language codes
-        if (rowCounter === 0) {
-            Object.keys(row)
-                .filter(k => k !== 'key')
-                // create dictionary for each language
-                .map(k => {
-                    locales[k] = {};
-                });
-        }
+for (const file of fs.readdirSync(inDirectory)) {
+    if (file.indexOf('.json') < 0) continue;
+    const lang = file.split('.json', 1).shift(),
+        inPath = path.join(inDirectory, file),
+        outPath = path.join(outDirectory, lang),
+        content = JSON.parse(fs.readFileSync(inPath, 'utf-8')),
+        obj = {};
 
-        // subsequent rows contain data
-        if (rowCounter > 0 && row.key) {
+    if (lang === 'en') writeMenuLinks(content);
 
-            // remove invalid chars from i18n dictionary key
-            const k = row.key
-                .replace(/-/g, '_')
-                .replace(/\//g, '_');
+    Object.keys(content)
+        .filter(key => content[key])
+        .map(key => (obj[sanitizedKey(key)] = {'message': content[key]}));
 
-            // add dictionary entry for each language,
-            // if localized value exists
-            Object.keys(locales).map(locale => {
-                if (row[locale]) locales[locale][k] = {'message': row[locale]};
-            });
-
-        }
-
-        // use empty row to indicate start of menu links
-        if (rowCounter > 1 && !row.key) {
-            menuLinksStartRow = true;
-        }
-
-        // record all entries that represent links
-        // in their original form
-        if (menuLinksStartRow && row.key) {
-            keys.push(row.key);
-        }
-
-        rowCounter++;
-    })
-
-    // after reading all CSV rows
-    .on('end', _ => {
-
-        clearDirectory(outPath);
-
-        // generate updated language dictionaries
-        Object.keys(locales)
-            // only take non-empty dictionaries
-            .filter(k => (k && Object.values(locales[k]).length))
-            // write locales directories
-            .map(language => writeFile(outPath + language,
-                outFileName, locales[language]));
-
-        // write all links to file for use by source
-        writeFile('./src/', 'links.json', {'MenuLinks': keys});
-
-        console.log(` CSV file processed successfully with ${keys.length} links`);
-        console.log(' Check that xt-build.locales_list matches ' + outPath + '\n');
-
-    });
+    clearDirectory(outPath);
+    writeJSONFile(outPath, outFileName, obj);
+    console.log(`translated ${lang}:`, Object.keys(obj).length, 'terms');
+}
