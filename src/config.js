6b7235fc1a95d48e66580530b9ce19a73d9bf768
Neea Rusch <neea.rusch@mobilefirst.me>
2021-07-19 17:11:47 -0400

    minor doc changes

--- src/config.js
+++ src/config.js
@@ -1,13 +1,7 @@
-/** * * * * * * * * * * * * * * * * * * * *
- * Shortcuts for Chrome
- * Custom navigation menu for Chrome browser
- *
- * Author: Mobile First LLC
- * Website: https://mobilefirst.me
- *
+/**
  * @description
  * Extension configuration options
- * * * * * * * * * * * * * * * * * * * * */
+ */
 
 /**
  * Various application configs
--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,7 @@
-/** * * * * * * * * * * * * * * * * * * * *
- * Shortcuts for Chrome
- * Custom navigation menu for Chrome browser
- *
- * Author: Mobile First LLC
- * Website: https://mobilefirst.me
- *
+/**
  * @description
  * Runner for popup window script
- * * * * * * * * * * * * * * * * * * * * */
+ */
 
 import Popup from './popup/popup';
 
