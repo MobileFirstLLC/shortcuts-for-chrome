a51706e618936826399ecea3973e4491f1baf75f
Neea Rusch <neea.rusch@mobilefirst.me>
2023-11-18 16:29:44 -0500

    remove share options from context menu (#96)

--- test/context.spec.js
+++ test/context.spec.js
@@ -17,21 +17,14 @@ describe('Context Menu', () => {
         sandbox.restore();
     });
 
-    it('It opens source code link', () => {
+    it('Click opens source code link', () => {
         const args = {url: ContextMenuOptions.source.url};
         expect(chrome.tabs.create.withArgs(args).notCalled).to.be.true;
         chrome.contextMenus.onClicked.dispatch({menuItemId: 'source'});
         expect(chrome.tabs.create.withArgs(args).calledOnce).to.be.true;
     });
 
-    it('It opens social links', () => {
-        const url = ContextMenu.generateUrl(ContextMenuOptions.twitter);
-        expect(chrome.tabs.create.withArgs({url}).notCalled).to.be.true;
-        chrome.contextMenus.onClicked.dispatch({menuItemId: 'twitter'});
-        expect(chrome.tabs.create.withArgs({url}).calledOnce).to.be.true;
-    });
-
-    it('It ignores invalid click', () => {
+    it('Invalid action is ignored', () => {
         expect(chrome.tabs.create.notCalled).to.be.true;
         chrome.contextMenus.onClicked.dispatch({menuItemId: 'invalid'});
         expect(chrome.tabs.create.notCalled).to.be.true;
